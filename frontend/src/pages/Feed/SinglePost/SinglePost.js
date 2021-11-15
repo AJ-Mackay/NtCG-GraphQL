4242c55a5e6a26732c1fe283bc8325e528758cb5
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-15 21:20:26 +0000

    Using Variables

--- frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -9,14 +9,14 @@ class SinglePost extends Component {
     author: '',
     date: '',
     image: '',
-    content: ''
+    content: '',
   };
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
     const graphqlQuery = {
-      query: `{
-          post(id: "${postId}") {
+      query: `query FetchSinglePost($postid: ID!) {
+          post(id: $postId) {
             title
             content
             imageUrl
@@ -26,20 +26,23 @@ class SinglePost extends Component {
             createdAt
           }
         }
-      `
+      `,
+      variables: {
+        postId: postId,
+      },
     };
     fetch('http://localhost:8080/graphql', {
       method: 'POST',
       headers: {
         Authorization: 'Bearer ' + this.props.token,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(graphqlQuery)
+      body: JSON.stringify(graphqlQuery),
     })
-      .then(res => {
+      .then((res) => {
         return res.json();
       })
-      .then(resData => {
+      .then((resData) => {
         if (resData.errors) {
           throw new Error('Fetching post failed!');
         }
@@ -47,11 +50,13 @@ class SinglePost extends Component {
           title: resData.data.post.title,
           author: resData.data.post.creator.name,
           image: 'http://localhost:8080/' + resData.data.post.imageUrl,
-          date: new Date(resData.data.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.data.post.content
+          date: new Date(resData.data.post.createdAt).toLocaleDateString(
+            'en-US'
+          ),
+          content: resData.data.post.content,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
