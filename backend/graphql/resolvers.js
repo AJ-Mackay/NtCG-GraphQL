442658759489e786b6097c81cc158751e94c9c87
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-15 18:01:20 +0000

    Deleting Posts

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -4,9 +4,10 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 const Post = require('../models/post');
+const { clearImage } = require('../util/file');
 
 module.exports = {
-  createUser: async function ({ userInput }, req) {
+  createUser: async function({ userInput }, req) {
     //   const email = args.userInput.email;
     const errors = [];
     if (!validator.isEmail(userInput.email)) {
@@ -33,12 +34,12 @@ module.exports = {
     const user = new User({
       email: userInput.email,
       name: userInput.name,
-      password: hashedPw,
+      password: hashedPw
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
-  login: async function ({ email, password }) {
+  login: async function({ email, password }) {
     const user = await User.findOne({ email: email });
     if (!user) {
       const error = new Error('User not found.');
@@ -54,14 +55,14 @@ module.exports = {
     const token = jwt.sign(
       {
         userId: user._id.toString(),
-        email: user.email,
+        email: user.email
       },
       'somesupersecretsecret',
       { expiresIn: '1h' }
     );
     return { token: token, userId: user._id.toString() };
   },
-  createPost: async function ({ postInput }, req) {
+  createPost: async function({ postInput }, req) {
     if (!req.isAuth) {
       const error = new Error('Not authenticated!');
       error.code = 401;
@@ -96,7 +97,7 @@ module.exports = {
       title: postInput.title,
       content: postInput.content,
       imageUrl: postInput.imageUrl,
-      creator: user,
+      creator: user
     });
     const createdPost = await post.save();
     user.posts.push(createdPost);
@@ -105,10 +106,10 @@ module.exports = {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
       createdAt: createdPost.createdAt.toISOString(),
-      updatedAt: createdPost.updatedAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString()
     };
   },
-  posts: async function ({ page }, req) {
+  posts: async function({ page }, req) {
     if (!req.isAuth) {
       const error = new Error('Not authenticated!');
       error.code = 401;
@@ -125,18 +126,18 @@ module.exports = {
       .limit(perPage)
       .populate('creator');
     return {
-      posts: posts.map((p) => {
+      posts: posts.map(p => {
         return {
           ...p._doc,
           _id: p._id.toString(),
           createdAt: p.createdAt.toISOString(),
-          updatedAt: p.updatedAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString()
         };
       }),
-      totalPosts: totalPosts,
+      totalPosts: totalPosts
     };
   },
-  post: async function ({ id }, req) {
+  post: async function({ id }, req) {
     if (!req.isAuth) {
       const error = new Error('Not authenticated!');
       error.code = 401;
@@ -152,13 +153,13 @@ module.exports = {
       ...post._doc,
       _id: post._id.toString(),
       createdAt: post.createdAt.toISOString(),
-      updatedAt: post.updatedAt.toISOString(),
+      updatedAt: post.updatedAt.toISOString()
     };
   },
-  updatePost: async function ({ id, postInput }, req) {
+  updatePost: async function({ id, postInput }, req) {
     if (!req.isAuth) {
-      const error = new Error('Not Authenticated!');
-      error.code = 404;
+      const error = new Error('Not authenticated!');
+      error.code = 401;
       throw error;
     }
     const post = await Post.findById(id).populate('creator');
@@ -168,7 +169,7 @@ module.exports = {
       throw error;
     }
     if (post.creator._id.toString() !== req.userId.toString()) {
-      const error = new Error('Not Authorised!');
+      const error = new Error('Not authorized!');
       error.code = 403;
       throw error;
     }
@@ -201,7 +202,31 @@ module.exports = {
       ...updatedPost._doc,
       _id: updatedPost._id.toString(),
       createdAt: updatedPost.createdAt.toISOString(),
-      updatedAt: updatedPost.updatedAt.toISOString(),
+      updatedAt: updatedPost.updatedAt.toISOString()
     };
   },
+  deletePost: async function({ id }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const post = await Post.findById(id);
+    if (!post) {
+      const error = new Error('No post found!');
+      error.code = 404;
+      throw error;
+    }
+    if (post.creator.toString() !== req.userId.toString()) {
+      const error = new Error('Not authorized!');
+      error.code = 403;
+      throw error;
+    }
+    clearImage(post.imageUrl);
+    await Post.findByIdAndRemove(id);
+    const user = await User.findById(req.userId);
+    user.posts.pull(id);
+    await user.save();
+    return true;
+  }
 };
