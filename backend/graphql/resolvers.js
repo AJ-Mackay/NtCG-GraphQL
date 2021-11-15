97baec2ba039c59a5b23cae89b55109bf97d9cb8
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-15 11:57:12 +0000

    Sending the 'Create Post' Query

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -6,11 +6,11 @@ const User = require('../models/user');
 const Post = require('../models/post');
 
 module.exports = {
-  createUser: async function ({ userInput }, req) {
-    // const email = args.userInput.email;
+  createUser: async function({ userInput }, req) {
+    //   const email = args.userInput.email;
     const errors = [];
     if (!validator.isEmail(userInput.email)) {
-      errors.push({ message: 'Email is invalid.' });
+      errors.push({ message: 'E-Mail is invalid.' });
     }
     if (
       validator.isEmpty(userInput.password) ||
@@ -33,12 +33,12 @@ module.exports = {
     const user = new User({
       email: userInput.email,
       name: userInput.name,
-      password: hashedPw,
+      password: hashedPw
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
-  login: async function ({ email, password }) {
+  login: async function({ email, password }) {
     const user = await User.findOne({ email: email });
     if (!user) {
       const error = new Error('User not found.');
@@ -47,22 +47,22 @@ module.exports = {
     }
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      const error = new Error('Passowrd is incorrect.');
+      const error = new Error('Password is incorrect.');
       error.code = 401;
       throw error;
     }
     const token = jwt.sign(
       {
         userId: user._id.toString(),
-        email: user.email,
+        email: user.email
       },
       'somesupersecretsecret',
       { expiresIn: '1h' }
     );
     return { token: token, userId: user._id.toString() };
   },
-  createPost: async function ({ postInput }, req) {
-    if (!res.isAuth) {
+  createPost: async function({ postInput }, req) {
+    if (!req.isAuth) {
       const error = new Error('Not authenticated!');
       error.code = 401;
       throw error;
@@ -86,7 +86,7 @@ module.exports = {
       error.code = 422;
       throw error;
     }
-    const user = await User.findById(red.userId);
+    const user = await User.findById(req.userId);
     if (!user) {
       const error = new Error('Invalid user.');
       error.code = 401;
@@ -96,15 +96,38 @@ module.exports = {
       title: postInput.title,
       content: postInput.content,
       imageUrl: postInput.imageUrl,
-      creator: user,
+      creator: user
     });
     const createdPost = await post.save();
     user.posts.push(createdPost);
+    await user.save();
     return {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
       createdAt: createdPost.createdAt.toISOString(),
-      updatedAt: createdPost.updatedAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString()
     };
   },
+  posts: async function(args, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const totalPosts = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate('creator');
+    return {
+      posts: posts.map(p => {
+        return {
+          ...p._doc,
+          _id: p._id.toString(),
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString()
+        };
+      }),
+      totalPosts: totalPosts
+    };
+  }
 };
--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -15,7 +15,7 @@ module.exports = buildSchema(`
         _id: ID!
         name: String!
         email: String!
-        password: String!
+        password: String
         status: String!
         posts: [Post!]!
     }
@@ -23,6 +23,11 @@ module.exports = buildSchema(`
     type AuthData {
         token: String!
         userId: String!
+    }
+
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
     }
 
     input UserInputData {
@@ -39,12 +44,13 @@ module.exports = buildSchema(`
 
     type RootQuery {
         login(email: String!, password: String!): AuthData!
+        posts: PostData!
     }
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
         createPost(postInput: PostInputData): Post!
-    }    
+    }
 
     schema {
         query: RootQuery
